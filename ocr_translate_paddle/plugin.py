e096a6bf4b055d2606e6a9410d3f8edff88f6e37
crivella <davide.grassano@epfl.ch>
2024-03-25 17:35:41 +0100

    WIP

---
+++ ocr_translate_paddle/plugin.py
@@ -0,0 +1,78 @@
+###################################################################################
+# ocr_translate-hugging_face - a plugin for ocr_translate                         #
+# Copyright (C) 2023-present Davide Grassano                                      #
+#                                                                                 #
+# This program is free software: you can redistribute it and/or modify            #
+# it under the terms of the GNU General Public License as published by            #
+# the Free Software Foundation, either version 3 of the License.                  #
+#                                                                                 #
+# This program is distributed in the hope that it will be useful,                 #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                  #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   #
+# GNU General Public License for more details.                                    #
+#                                                                                 #
+# You should have received a copy of the GNU General Public License               #
+# along with this program.  If not, see {http://www.gnu.org/licenses/}.           #
+#                                                                                 #
+# Home: https://github.com/Crivella/ocr_translate-hugging_face                    #
+###################################################################################
+"""ocr_translate plugin to allow loading of PaddleOCR."""
+import logging
+
+import numpy as np
+from ocr_translate import models as m
+from paddleocr import PaddleOCR
+from PIL import Image
+
+logger = logging.getLogger('plugin')
+
+class HugginfaceVEDModel(m.OCRModel):
+    """OCRtranslate plugin to allow loading of PaddleOCR as text OCR."""
+    class Meta: # pylint: disable=missing-class-docstring
+        proxy = True
+
+    def load(self):
+        """Load the model into memory."""
+        logger.debug('Loading PaddleOCR model (nothing to do here)')
+
+    def unload(self) -> None:
+        """Unload the model from memory."""
+        logger.debug('Unloading PaddleOCR model (nothing to do here)')
+
+    def _ocr(
+            self,
+            img: Image.Image, lang: str = None, options: dict = None
+            ) -> str:
+        """Perform OCR on an image.
+
+        Args:
+            img (Image.Image):  A Pillow image on which to perform OCR.
+            lang (str, optional): The language to use for OCR. (Not every model will use this)
+            bbox (tuple[int, int, int, int], optional): The bounding box of the text on the image in lbrt format.
+            options (dict, optional): A dictionary of options to pass to the OCR model.
+
+        Raises:
+            TypeError: If img is not a Pillow image.
+
+        Returns:
+            str: The text extracted from the image.
+        """
+
+        ocr = PaddleOCR(use_angle_cls=True, lang=lang) # need to run only once to download and load model into memory
+        result = ocr.ocr(np.array(img), cls=True)[0]
+
+        logger.debug(f'PaddleOCR result: {result}')
+
+        generated_text = []
+        for line in result:
+            # box = np.array(line[0])
+            # l = box[:, 0].min()
+            # t = box[:, 1].min()
+            # r = box[:, 0].max()
+            # b = box[:, 1].max()
+            text, _ = line[1]
+            generated_text.append(text)
+
+        generated_text = ' '.join(generated_text)
+
+        return generated_text
---
+++ tests/settings.py
@@ -0,0 +1,7 @@
+"""Minimal django settings for tests."""
+INSTALLED_APPS = [
+    'ocr_translate',
+    'ocr_translate_paddle'
+]
+
+USE_TZ = True
